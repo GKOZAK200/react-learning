22bb343cf2fb22008f5e94084426e631182c201d
GKOZAK200 <GKOZAK200@caledonian.ac.uk>
2022-05-08 17:47:18 +0300

    Added Description component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ function Title() {
   );
 }
 
+function Description() {
+  return (
+      <p>This website was designed to allow me to study and practice developing with the React framework</p>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <Title />
+      <Description />
     </div>
   );
 }
